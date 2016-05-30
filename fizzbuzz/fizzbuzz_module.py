25fff9b4e52a6fef546eb011d9e000d9b5f79ace
Mate Gonczi <gamte19@gmail.com>
2016-05-30 13:10:51 +0200

    done

--- fizzbuzz/fizzbuzz_module.py
+++ fizzbuzz/fizzbuzz_module.py
@@ -1,8 +1,18 @@
 def fizzbuzz(number):
-    return
+    to_return = number
+    if number % 15 == 0:
+        to_return = "FizzBuzz"
+    elif number % 3 == 0:
+        to_return = "Fizz"
+    elif number % 5 == 0:
+        to_return = "Buzz"
+    return to_return
 
 
 def main():
+    for i in range(1, 101):
+        num = fizzbuzz(i)
+        print(num)
     return
 
 if __name__ == '__main__':

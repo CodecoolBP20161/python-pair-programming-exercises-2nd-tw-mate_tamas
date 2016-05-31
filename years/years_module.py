8048cefae81cf383d55695e8372e049722949ff5
Mate Gonczi <gamte19@gmail.com>
2016-05-31 10:10:22 +0200

    done

--- years/years_module.py
+++ years/years_module.py
@@ -1,11 +1,22 @@
 import datetime
+from datetime import date
+
 
 
 def years(age):
-    return
+    n = 100 - age
+    after_hundred_years = date.today().year+n
+    after = after_hundred_years
+    return after
 
 
 def main():
+    name = input("What's your name? ")
+    age = int(input("How old are you? "))
+    years(age)
+    valami = years(age)
+    #after = after_hundred_years
+    print(name+" you will be 100 years old in: ", valami)
     return
 
 

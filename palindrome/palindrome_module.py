52855c23cecb5435cac08be4b6a2ab5ccb85a3d6
Mate Gonczi <gamte19@gmail.com>
2016-06-02 09:19:39 +0200

    done

--- palindrome/palindrome_module.py
+++ palindrome/palindrome_module.py
@@ -1,10 +1,13 @@
-def palindrome(str):
-    return
+def palindrome(string_input):
+    string_input = string_input.replace(" ", "").lower()
+    print(string_input[0:] == string_input[::-1])
+    return string_input[0:] == string_input[::-1]
 
 
 def main():
+    string_input = input("write a string:\n")
+    palindrome(string_input)
     return
-
 
 if __name__ == '__main__':
     main()
--- passwordgen/passwordgen_module.py
+++ passwordgen/passwordgen_module.py
@@ -1,10 +1,27 @@
+import random
+
+
 def passwordgen():
-    return
+    spec = ['!', '@', '#', '$', '%', '^', '&', ',', '*', '(', ')', '?']
+    number = []
+    upper = []
+    lower = []
+    password = []
+    for i in range(65, 91):
+        upper.append(chr(i))
+    for i in range(97, 123):
+        lower.append(chr(i))
+    for i in range(48, 58):
+        number.append(chr(i))
+    password = random.sample(spec, 2) + random.sample(number, 2) + random.sample(upper, 2)+random.sample(lower, 2)
+    password = ''.join(password)
+    print(password)
+    return password
 
 
 def main():
+    passwordgen()
     return
-
 
 if __name__ == '__main__':
     main()
